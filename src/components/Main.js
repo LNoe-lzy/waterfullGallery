c049aea3699044459c4c7c3928ffb29ab5218280
LNoe-lzy <m15222505516@163.com>
2016-09-10 17:05:53 +0800

    解决图片加载问题。

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 let imgData = require('../data/imgdata.json');
 
@@ -15,6 +16,10 @@ imgData = ((imgArr) => {
 })(imgData);
 
 class ImgFigure extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
   render () {
     return (
       <figure className="img-item">
@@ -25,13 +30,54 @@ class ImgFigure extends React.Component {
 }
 
 class AppComponent extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.stageOnload = this.stageOnload.bind(this);
+  }
+
+  getMinIndex (arr, value) {
+    for (let i in arr) {
+      if (arr[i] === value) {
+        return i;
+      }
+    }
+  }
+
+  stageOnload () {
+    // 获取全部的图片元素
+    let imgArray = [];
+    let stageDOM = ReactDOM.findDOMNode(this.refs.stage);
+    for (let i = 0,j = imgData.length; i < j; i ++) {
+      let ref = this.refs['img' + i];
+      imgArray.push(ReactDOM.findDOMNode(ref));
+    }
+    // 计算整个页面显示的页数
+    let stageW = stageDOM.offsetWidth,
+      itemW = imgArray[0].offsetWidth,
+      cols = Math.floor(stageW / itemW),
+
+      // 存放列高度的数组
+      heightArr = [];
+    for (let i = 0, j = imgArray.length; i < j; i++) {
+      if (i < cols) {
+        heightArr.push(imgArray[i].offsetHeight);
+      } else {
+        let minH = Math.min.apply(null, heightArr),
+          index = this.getMinIndex(heightArr, minH);
+        imgArray[i].style.position = 'absolute';
+        imgArray[i].style.top = minH + 20 + 'px';
+        imgArray[i].style.left = (itemW + 20) * index + 'px';
+      }
+    }
+  }
   render() {
     let imgItem = [];
     imgData.forEach((value, index) => {
-      imgItem.push(<ImgFigure data={value} key={index} />)
+      imgItem.push(<ImgFigure data={value} key={index} ref={'img' + index} />)
     });
     return (
-      <section className="stage">
+      <section className="stage" ref="stage" onLoad={this.stageOnload}>
         <section className="img-section">
           {imgItem}
         </section>
